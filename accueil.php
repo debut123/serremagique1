82924da8903f299efe688293f4792c56f46ce427
debut123 <160791890+debut123@users.noreply.github.com>
2025-06-12 10:00:11 +0200

    te

--- accueil.php
+++ accueil.php
@@ -9,23 +9,6 @@ if (!isset($_SESSION['id'])) {
 }
 
 
-
-try {
-    // Paramètres de connexion à la base distante
-    $host = 'herogu.garageisep.com';           // Exemple : 'sql8.freemysqlhosting.net'
-    $dbname = '8tQjb0ov2H_serre_auto';
-    $username = 'CpZoJWSnPV_serre_auto';    // Exemple : '8tQjb0ov2H'
-    $password = 'XqUTQ7FjLH2yql3X';   // Mot de passe réel
-
-    // Connexion PDO
-    $pdo_dist = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
-    $pdo_dist->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-    // Test simple (à supprimer après validation)
-    echo "<p style='color:green;'>Connexion à la base distante réussie </p>";
-} catch (PDOException $e) {
-    die("<p style='color:red;'>Erreur de connexion à la base distante  : " . $e->getMessage() . "</p>");
-}
 ?>
 
 <!DOCTYPE html>
